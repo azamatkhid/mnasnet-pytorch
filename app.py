7574a0c52304a1f3c6974adc322ea164667534b3
azamatkhid <khassenov.azamat@gmail.com>
2020-06-05 11:19:46 +0900

    lr decay scheduler + save & load ckpts (#2)

--- app.py
+++ app.py
@@ -33,38 +33,38 @@ class Application:
 
         self.test_transforms = transforms.Compose([transforms.Resize((224, 224), interpolation=2),
             transforms.ToTensor(),
-            transforms.Normalize(mean=[0.5, 0.5, 0.5],
-                std=[0.5, 0.5, 0.5])])
+            transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])])
+
+        print(f"[*] Device: {self.device}")
+        print(f"[*] Path: {self.data_dir}")
 
         self.net = None
         
-    def build(self):
+    def build(self, criterion=nn.CrossEntropyLoss, optimizer=torch.optim.RMSprop):
         self.net = MnasNet(3, self.cfg, training=True)
-
         self.net = nn.DataParallel(self.net)
         
         if torch.cuda.device_count() > 1:
-            print(f"Number of GPUs : {torch.cuda.device_count()}")
+            print(f"[*] Number of GPUs : {torch.cuda.device_count()}")
 
         self.net.to(self.device)
         if self.cfg.verbose and torch.cuda.device_count() <= 1:
             summary(self.net, (3, 224, 224))
-        pass
 
-    def train(self, criterion=nn.CrossEntropyLoss, optimizer=torch.optim.RMSprop):
-        self.net.train()
+        self.criterion = criterion()
+        self.optimizer = optimizer(self.net.parameters(), lr=self.cfg.lr_init)
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.5,
+                patience=1, verbose=False, threshold=0.0001, threshold_mode='abs', cooldown=0, min_lr=1e-8, eps=1e-08)
+
         self._check_dirs()
         self._load_data("train")
-        
+        self._load_ckpts()
         self.writer = SummaryWriter(log_dir=self.cfg.log_dir)
-        self.criterion = criterion()
-        self.optimizer = optimizer(self.net.parameters(), lr=self.cfg.lr,
-                momentum=self.cfg.momentum, weight_decay=self.cfg.weight_decay)
-        self.scheduler = torch.optim.lr_scheduler.StepLR(self.optimizer,
-                step_size=self.cfg.lr_step, gamma=self.cfg.lr_gamma)
-
-        iteration = 1
-        for epch in range(self.cfg.epochs):
+ 
+    def train(self):
+        self.net.train()
+        iteration = self.step
+        for epch in range(self.start_epoch, self.cfg.epochs):
             running_loss = 0.
             epch_loss = 0.
             
@@ -83,19 +83,18 @@ class Application:
                     self.writer.add_scalar("Loss/Train", running_loss/self.cfg.verbose_step, iteration)
                     self.writer.add_scalar("Loss/Validation", valid_loss, iteration)
                     self.writer.add_scalar("Acc/Validation", valid_acc, iteration)
-                    self.writer.add_scalar("LearningRate", self.scheduler.get_lr()[0], iteration)
-                    print(f"{epch} Loss/train: {running_loss/self.cfg.verbose_step}, Loss/val: {valid_loss}, ", 
-                            f"Acc/val: {valid_acc}, lr: {self.scheduler.get_lr()[0]}")
+                    self.writer.add_scalar("LearningRate", self.optimizer.param_groups[0]["lr"], iteration)
+                    ckpt_name = f"step_{iteration}_val_loss_{valid_loss:.3}_{valid_acc:.2f}.pth"
+                    self._save_ckpts(iteration, epch, ckpt_name) 
+
+                    print(f"[{epch}/{iteration}] Loss/train: {running_loss/self.cfg.verbose_step:.3}, Loss/val: {valid_loss:.3}, Acc/val: {valid_acc:7.2f}, lr: {self.optimizer.param_groups[0]['lr']:.3}")
                     running_loss = 0.
                     iteration += 1
 
-            self.scheduler.step()
-
-        torch.save(self.net.state_dict(),os.path.join(self.cfg.ckpts_dir,"model.pth"))
+            self.scheduler.step(metrics=valid_loss)
 
     def test(self):
         self.net.eval()
-        self._check_dirs()
         self._load_data("test")
 
         correct = 0
@@ -107,9 +106,9 @@ class Application:
                 outputs = self.net(inputs)
                 _, predicted = torch.max(outputs, 1)
                 total += labels.size(0)
-                correct = (predicted == labels).sum().item()
+                correct += (predicted == labels).sum().item()
         acc = correct / total * 100
-        print(f"Test accuracy: {acc}%")
+        print(f"[*] Test accuracy: {acc:.2f}%")
 
         self.net.train()
 
@@ -128,11 +127,46 @@ class Application:
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
         self.net.train()
-        acc = correct / total
+        acc = correct / total * 100
         batch_loss = total_loss / len(self.valid_data)
         return acc, batch_loss
 
-        pass
+
+    def _save_ckpts(self, step, epoch, filename):
+        state = {"network": self.net.state_dict(),
+                "optimizer": self.optimizer.state_dict(),
+                "step": step,
+                "epoch": epoch}
+        torch.save(state, os.path.join(self.cfg.ckpts_dir, filename))
+
+
+    def _load_ckpts(self):
+        self.step = 1
+        self.start_epoch = 0
+        if os.path.exists(self.cfg.resume):
+            resume_path = self.cfg.resume
+        else:
+            ckpts = [[f, int(f.split("_")[1])] for f in os.listdir(self.cfg.ckpts_dir) if f.endswith(".pth")]
+            ckpts.sort(key = lambda x: x[1], reverse=True)
+            resume_path = None if len(ckpts) == 0 else os.path.join(self.cfg.ckpts_dir, ckpts[0][0])
+
+        if resume_path and os.path.exists(resume_path):
+            print(f"[*] CKPTs Loading {resume_path}")
+            checkpoint = torch.load(resume_path, map_location="cpu")
+            self.step = checkpoint["step"] + 1
+            self.net.load_state_dict(checkpoint["network"])
+
+            if not self.cfg.optimizer_reset:
+                self.optimizer.load_state_dict(checkpoint["optimizer"])
+
+            if "epoch" in checkpoint:
+                self.start_epoch = checkpoint["epoch"]
+
+            print(f"[*] CKPTs Loaded {resume_path} (Continue from epoch {self.start_epoch} and step {self.step})")
+            del checkpoint
+            torch.cuda.empty_cache()
+        else:
+            print(f"[*] No CKPTs found at {resume_path}")
 
     def _check_dirs(self):
         if not os.path.exists(self.cfg.log_dir):
@@ -173,7 +207,8 @@ class Application:
 def main(cfg):
     app=Application(cfg.parameters)
     app.build()
-    app.train()
+    if not cfg.parameters.inference:
+        app.train()
     app.test()
 
 
--- ref_app.py
+++ ref_app.py
@@ -1,5 +1,7 @@
 import torch
+import torch.nn as nn
 from torchvision import models
+from torch.utils.tensorboard import SummaryWriter
 from torchsummary import summary
 import hydra
 
@@ -10,26 +12,35 @@ class MnasNet_official(Application):
         super(MnasNet_official, self).__init__(cfg)
         pass
 
-    def build(self):
-        if self.cfg.model=="mnasnet-b1":
-            model = getattr(models, "mnasnet1_0")
+    def build(self, criterion=nn.CrossEntropyLoss, optimizer=torch.optim.RMSprop):
+        model = getattr(models, "mnasnet1_0")
         self.net = model(pretrained = False, num_classes = self.cfg.num_classes)
-
         self.net = torch.nn.DataParallel(self.net)
         
         if torch.cuda.device_count() > 1:
-            print(f"Number of GPUs: {torch.cuda.device_count()}")
+            print(f"[*] Number of GPUs: {torch.cuda.device_count()}")
 
         self.net.to(self.device)
         if self.cfg.verbose and torch.cuda.device_count() <= 1:
             summary(self.net,(3,224,224))
+        
+        self.criterion = criterion()
+        self.optimizer = optimizer(self.net.parameters(), lr=self.cfg.lr_init)
+        self.scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(self.optimizer, mode='min', factor=0.5,
+                patience=1, verbose=False, threshold=0.0001, threshold_mode='abs', cooldown=0, min_lr=1e-8, eps=1e-08)
+        
+        self._check_dirs()
+        self._load_data("train")
+        self._load_ckpts()
+        self.writer = SummaryWriter(log_dir=self.cfg.log_dir)
 
 
 @hydra.main(config_path="./default.yaml")
 def main(cfg):
     app=MnasNet_official(cfg.parameters)
     app.build()
-    app.train()
+    if not cfg.parameters.inference:
+        app.train()
     app.test()
 
 if __name__=="__main__":
